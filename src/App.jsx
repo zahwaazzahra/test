77795566506fc31d39ca8ab7fcbf60ad4658c2c5
zahwaazzahra <zahwa.122140102@student.itera.ac.id>
2025-04-05 19:40:45 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Rent from './pages/rent'; // perhatikan huruf kapital 'R'
+import Payment from './pages/Payment';
+import Confirmation from './pages/Confirmation';
+import Navbar from './components/Navbar';
+import { SepedaProvider } from './context/SepedaContext';
+
+function App() {
+  return (
+    <SepedaProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rent" element={<Rent />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/confirmation" element={<Confirmation />} />
+      </Routes>
+    </SepedaProvider>
+  );
+}
+
+export default App;
